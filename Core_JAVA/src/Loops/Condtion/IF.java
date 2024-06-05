344a8e8c435365afa121e26ba8508b3902619b68
Maheshbhart15 <127493536+Maheshbhart15@users.noreply.github.com>
2024-06-06 00:05:51 +0530

    IF

--- Core_JAVA/src/Loops/Condtion/IF.java
+++ Core_JAVA/src/Loops/Condtion/IF.java
@@ -1,9 +1,23 @@
 package Loops.Condtion;
 
+import java.util.Scanner;
+
 public class IF {
     public static void main(String[] args) {
         // if only check condtion based on true statement of code will be excute
         // will try for odd even
-        System.out.println("Enter nu");
+        System.out.println("Enter num check even or odd ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        if (num > 0) {
+            if (num / 2 == 0) {
+                System.out.println("EVEN NUM");
+            } else {
+                System.out.println("ODD NUM");
+            }
+        } else {
+            System.out.println("You enter num less then 0 or 0 ");
+        }
     }
+
 }
