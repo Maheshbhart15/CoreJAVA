6c2d43803620dcf9447951a7842ae85a8c654fcc
Maheshbhart15 <127493536+Maheshbhart15@users.noreply.github.com>
2024-06-06 00:01:23 +0530

    If

---
+++ Core_JAVA/src/Loops/Condtion/IF.java
@@ -0,0 +1,9 @@
+package Loops.Condtion;
+
+public class IF {
+    public static void main(String[] args) {
+        // if only check condtion based on true statement of code will be excute
+        // will try for odd even
+        System.out.println("Enter nu");
+    }
+}
