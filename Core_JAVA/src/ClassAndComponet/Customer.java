f15a1d4df730d02475bc6e503ed5f991ce0606da
Maheshbhart15 <127493536+Maheshbhart15@users.noreply.github.com>
2024-06-15 22:47:33 +0530

    Try again

---
+++ Core_JAVA/src/ClassAndComponet/Customer.java
@@ -0,0 +1,29 @@
+package ClassAndComponet;
+    public class Customer {
+        int id;
+        String name;
+        public static int customerCount;
+
+        {
+            System.out.println("instance block");
+            customerCount++;
+        }
+        static {
+            System.out.println("static block ");
+        }
+        public Customer(){
+        }
+
+        public Customer(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public Customer(int id) {
+            this.id = id;
+        }
+
+        public Customer(String name) {
+            this.name = name;
+        }
+    }
---
+++ Core_JAVA/src/ClassAndComponet/demo1.java
@@ -0,0 +1,45 @@
+package ClassAndComponet;
+import java.security.KeyStore;
+import java.util.*;
+
+public class demo1 {
+    public static void main(String[] args) {
+        // public static void main(String[] args) {
+
+         //   HashMap<String, Integer> map = new HashMap<>();
+           // map.put("abc", 2);
+            //map.put("def", 1);
+            //map.put("hij", 4);
+//map.put("klm", 6);
+    //        map.put("nop", 2);
+      //      map.put("qrs", 2);
+        //    map.put("tuv", 6);
+          //  map.put("wxy", 8);
+//            map.put("zab", 1);
+  //          map.put("cde", 5);
+       /*     map.put("fgh", 4);
+            map.put("ijk", 3);
+
+            HashMap<Integer, String> duplicatMap = new HashMap<>();
+
+            Set<Entry<String, Integer>> entrySet;
+            entrySet = map.entrySet();
+            Iterator<KeyStore.Entry<String, Integer>> iterator = entrySet.iterator();
+            while(iterator.hasNext()) {
+                Entry<String, Integer> entry = iterator.next();
+                String key = entry.getKey();
+                Integer value = entry.getValue();
+
+                if(duplicatMap.containsKey(value)) {
+                    duplicatMap.put(value, duplicatMap.get(value)+", "+key);
+                } else {
+                    duplicatMap.put(value, key);
+                }
+            }
+            /*
+        */
+            System.out.println("Interviwer ask me this ");
+
+        }
+    }
+
